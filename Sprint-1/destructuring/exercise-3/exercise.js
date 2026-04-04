e73d8b21d79f5b2a77222d3877ae63c910322b44
Daniel Aderibigbe <danieladeribigbe07@gmail.com>
2026-04-04 09:15:58 +0100

    completed all exercises

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -1,8 +1,24 @@
 let order = [
-  { itemName: "Hot cakes", quantity: 1, unitPricePence: 232 },
+  { itemName: "Hot Cakes", quantity: 1, unitPricePence: 232 },
   { itemName: "Apple Pie", quantity: 2, unitPricePence: 139 },
   { itemName: "Egg McMuffin", quantity: 1, unitPricePence: 280 },
   { itemName: "Sausage McMuffin", quantity: 1, unitPricePence: 300 },
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+console.log("QTY".padEnd(8) + "ITEM".padEnd(20) + "TOTAL");
+
+let total = 0;
+
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  const lineTotal = (quantity * unitPricePence) / 100;
+
+  total += lineTotal;
+
+  console.log(
+    `${quantity}`.padEnd(8) + itemName.padEnd(20) + lineTotal.toFixed(2)
+  );
+});
+
+console.log(`\nTotal: ${total.toFixed(2)}`);
